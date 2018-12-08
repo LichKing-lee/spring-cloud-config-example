166786fe3e26ecf57c180a156c1e334785de2935
ChangYong <lcy9002@naver.com>
2018-12-08 16:16:36 +0900

    Configuration config client

--- config-client/src/main/java/com/yong/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/yong/configclient/ConfigClientApplication.java
@@ -1,12 +1,26 @@
 package com.yong.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@RestController
+	public static class Controller {
+		@Value("${example.phase}")
+		private String config;
+
+		@GetMapping
+		public String test() {
+			return config;
+		}
 	}
 }
