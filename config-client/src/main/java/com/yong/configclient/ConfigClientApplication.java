9648cbce5f6b76479e70324c7c1238ec8e3e552f
ChangYong <lcy9002@naver.com>
2018-12-08 16:33:27 +0900

    test

--- config-client/src/main/java/com/yong/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/yong/configclient/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.yong.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ public class ConfigClientApplication {
 	}
 
 	@RestController
+	@RefreshScope
 	public static class Controller {
 		@Value("${example.phase}")
 		private String config;
